2b994b7576682cfa6e7a0705bae0e395d7ba801c
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2025-03-12 07:49:08 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/misc/Fees.java
+++ src/main/java/seedu/address/model/misc/Fees.java
@@ -1,12 +1,26 @@
 package seedu.address.model.misc;
 
+/**
+ * Represents a fee with an integer value.
+ */
 public class Fees {
+    /** The value of the fee. */
     private final int value;
 
+    /**
+     * Constructs a {@code Fees} with the given value.
+     *
+     * @param value The value of the fee.
+     */
     public Fees(int value) {
         this.value = value;
     }
 
+    /**
+     * Returns the string representation of the fee, which is its value.
+     *
+     * @return The value of the fee as a string.
+     */
     @Override
     public String toString() {
         return String.valueOf(value);
--- src/main/java/seedu/address/model/person/Membership.java
+++ src/main/java/seedu/address/model/person/Membership.java
@@ -1,27 +1,39 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a person's membership status in the addressbook.
+ * Represents a person's membership status in the address book.
+ * The status can be one of the following: ACTIVE, EXPIRED, or NON-MEMBER.
  */
 public enum Membership {
     ACTIVE("ACTIVE"),
     EXPIRED("EXPIRED"),
     NONMEMBER("NON-MEMBER");
 
-    public final static String MESSAGE_CONSTRAINTS = "Membership status can only be: ACTIVE, EXPIRED, NON-MEMBER";
+    /** Message constraints for valid membership statuses. */
+    public static final String MESSAGE_CONSTRAINTS = "Membership status can only be: ACTIVE, EXPIRED, NON-MEMBER";
 
+    /** The status of the membership. */
     public final String status;
 
-
+    /**
+     * Constructs a {@code Membership} with the given status.
+     *
+     * @param status The status of the membership.
+     */
     private Membership(String status) {
         requireNonNull(status);
         this.status = status;
     }
 
-
+    /**
+     * Creates a {@code Membership} based on the given string.
+     *
+     * @param test The string to be converted to a membership status.
+     * @return The corresponding {@code Membership} status.
+     * @throws RuntimeException If the string does not match any valid membership status.
+     */
     public static Membership createMember(String test) throws RuntimeException {
         if (test.equals("ACTIVE")) {
             return ACTIVE;
@@ -34,9 +46,13 @@ public enum Membership {
         }
     }
 
+    /**
+     * Returns the string representation of the membership status.
+     *
+     * @return The string representation of the membership status.
+     */
     @Override
     public String toString() {
         return status;
     }
-
 }
