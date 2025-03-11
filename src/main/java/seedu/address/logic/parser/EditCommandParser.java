1c960085cfffc7e6056b6373138873d22c2427ad
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2025-03-12 06:55:39 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -50,7 +50,12 @@ public class EditCommandParser implements Parser<EditCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_MEMBER);
+        argMultimap.verifyNoDuplicatePrefixesFor(
+                PREFIX_NAME,
+                PREFIX_PHONE,
+                PREFIX_EMAIL,
+                PREFIX_ADDRESS,
+                PREFIX_MEMBER);
 
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
 
