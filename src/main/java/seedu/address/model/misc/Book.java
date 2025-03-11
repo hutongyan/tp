dcfc31afa424a71330224c35c2b99045af145102
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2025-03-11 10:27:00 +0800

    Fill in classes for Book and Fees It is minimal as I don't see any other command that requires more functionality in these two classes as of now.

--- src/main/java/seedu/address/model/misc/Book.java
+++ src/main/java/seedu/address/model/misc/Book.java
@@ -1,5 +1,14 @@
 package seedu.address.model.misc;
 
 public class Book {
+    private final String name;
 
+    public Book(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
--- src/main/java/seedu/address/model/misc/Fees.java
+++ src/main/java/seedu/address/model/misc/Fees.java
@@ -1,4 +1,14 @@
 package seedu.address.model.misc;
 
 public class Fees {
+    private final int value;
+
+    public Fees(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
 }
