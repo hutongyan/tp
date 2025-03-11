fb72a70f290dcf316fd52b810b37c78b1bdaeb2f
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2025-03-11 22:48:46 +0800

    Fix bug in EditCommandParser

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -62,7 +62,7 @@ public class EditCommandParser implements Parser<EditCommand> {
             editPersonDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
         }
         if (argMultimap.getValue(PREFIX_MEMBER).isPresent()) {
-            editPersonDescriptor.setMember(ParserUtil.parseMembership(argMultimap.getValue(PREFIX_MEMBER).get()));
+            editPersonDescriptor.setMembership(ParserUtil.parseMembership(argMultimap.getValue(PREFIX_MEMBER).get()));
         }
         parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
 
