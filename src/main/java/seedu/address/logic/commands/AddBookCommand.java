71b8f98b0664222ba5f375702e78806dc7591a36
Kiranlimtl <kiranlimtl@gmail.com>
2025-03-19 14:25:07 +0800

    Fix merge conflicts

--- src/main/java/seedu/address/logic/commands/AddBookCommand.java
+++ src/main/java/seedu/address/logic/commands/AddBookCommand.java
@@ -19,73 +19,37 @@ public class AddBookCommand extends Command {
             + "Parameters: " + PREFIX_BOOK + "BOOK NAME" + "\n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_BOOK + " Percy Jackson";
     public static final String MESSAGE_ADD_BOOK_SUCCESS = "Added Book: %1$s";
-<<<<<<< HEAD
     public static final String MESSAGE_ADD_BOOK_FAIL = "This book already exists in book list.";
     private final Book book;
 
     /**
      * Creates an AddBookCommand to add the specified {@code Book}
      */
-=======
-    public static final String MESSAGE_ADD_BOOK_FAIL = "Book: %1$s already exists in book list.";
-
-    private final Book book;
-
->>>>>>> upstream/master
     public AddBookCommand(Book book) {
         requireNonNull(book);
         this.book = book;
     }
-<<<<<<< HEAD
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         if (model.hasBook(book)) {
             throw new CommandException(String.format(MESSAGE_ADD_BOOK_FAIL, Messages.format(book)));
-=======
-
-    @Override
-    public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        List<Book> lastShownList = model.getFilteredBookList();
-
-        for (Book book : lastShownList) {
-            if (this.book.getName().toString().equals(book.getName().toString())) {
-                throw new CommandException(String.format(MESSAGE_ADD_BOOK_FAIL, Messages.format(book)));
-            }
->>>>>>> upstream/master
         }
 
         model.addBook(book);
         return new CommandResult(String.format(MESSAGE_ADD_BOOK_SUCCESS, Messages.format(book)));
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/master
     @Override
     public boolean equals(Object other) {
         if (other == this) {
             return true;
         }
-<<<<<<< HEAD
         if (!(other instanceof AddBookCommand)) {
             return false;
         }
         AddBookCommand otherAddCommand = (AddBookCommand) other;
         return book.equals(otherAddCommand.book);
     }
-=======
-
-        if (!(other instanceof AddBookCommand)) {
-            return false;
-        }
-
-        AddBookCommand otherAddCommand = (AddBookCommand) other;
-        return book.equals(otherAddCommand.book);
-    }
-
->>>>>>> upstream/master
     @Override
     public String toString() {
         return new ToStringBuilder(this)
