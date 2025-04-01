5c2e3d1258871a8f599483c69f9d7da9f0d3574b
Kiranlimtl <kiranlimtl@gmail.com>
2025-04-01 16:50:15 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/ListBorrowedBooksCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListBorrowedBooksCommandParserTest.java
@@ -24,24 +24,28 @@ public class ListBorrowedBooksCommandParserTest {
     @Test
     public void parse_invalidArgs_throwsParseException() {
         String userInput = " invalid args";
-        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListBorrowedBooksCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ListBorrowedBooksCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_missingEmail_throwsParseException() {
         String userInput = " ";
-        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListBorrowedBooksCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ListBorrowedBooksCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_duplicateEmailPrefix_throwsParseException() {
         String userInput = PREFIX_EMAIL + "example@example.com " + PREFIX_EMAIL + "duplicate@example.com";
-        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListBorrowedBooksCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ListBorrowedBooksCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_invalidPrefix_throwsParseException() {
         String userInput = PREFIX_BOOK + "invalidPrefix@example.com";
-        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListBorrowedBooksCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ListBorrowedBooksCommand.MESSAGE_USAGE));
     }
 }
