e0eb6aa8fc231558ea22d430cb53308485af9467
Charvi Priyadarshi <>
2025-04-03 20:54:01 +0800

    Enhance 'help' command to display list of all available commands in the application window

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -3,19 +3,53 @@ package seedu.address.logic.commands;
 import seedu.address.model.Model;
 
 /**
- * Format full help instructions for every command for display.
+ * Lists all available commands and their usage formats.
  */
 public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
-            + "Example: " + COMMAND_WORD;
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows list of all commands and usage.\n"
+                                               + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String HELP_MESSAGE = "List of available commands:\n"
+                                              + "1. add: Adds a user.\n"
+                                              + "   Format: add n/NAME p/PHONE e/EMAIL "
+                                              + "a/ADDRESS m/MEMBERSHIP [t/TAG]...\n"
+                                              + "2. edit: Edits an existing user.\n"
+                                              + "   Format: edit INDEX [n/NAME] [p/PHONE] "
+                                              + "[e/EMAIL] [a/ADDRESS] [m/MEMBERSHIP] [t/TAG]...\n"
+                                              + "3. delete: Deletes a user.\n"
+                                              + "   Format: delete INDEX\n"
+                                              + "4. list: Lists all users with optional filters.\n"
+                                              + "   Format: list_users [e/EMAIL] [n/NAME] "
+                                              + "[m/MEMBERSHIP] [b/BOOK] [t/TAG]\n"
+                                              + "5. find: Finds users by name.\n"
+                                              + "   Format: find KEYWORD [MORE_KEYWORDS]...\n"
+                                              + "6. add_book: Adds a book.\n"
+                                              + "   Format: add_book b/BOOK_NAME\n"
+                                              + "7. list_books: Lists all books.\n"
+                                              + "   Format: list_books\n"
+                                              + "8. delete_book: Deletes a book.\n"
+                                              + "   Format: delete_book INDEX\n"
+                                              + "9. issue: Issues a book to a user.\n"
+                                              + "   Format: issue b/BOOK_NAME e/EMAIL d/DATE\n"
+                                              + "10. return: Returns a book from a user.\n"
+                                              + "    Format: return b/BOOK_NAME d/RETURN_DATE\n"
+                                              + "11. list_borrowed_books: Lists borrowed books by user.\n"
+                                              + "    Format: list_borrowed_books e/EMAIL\n"
+                                              + "12. display_overdue: Displays users with overdue books.\n"
+                                              + "    Format: display_overdue\n"
+                                              + "13. clear: Clears all users and books.\n"
+                                              + "    Format: clear\n"
+                                              + "14. exit: Exits the app.\n"
+                                              + "    Format: exit\n"
+                                              + "15. help: Shows this help message.\n"
+                                              + "    Format: help";
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        return new CommandResult(HELP_MESSAGE);
     }
+
 }
